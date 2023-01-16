cfd6fe3940dadde06893eaea6640ea0ed505630b
Michał Gapiński <mike@gapinski.eu>
2023-01-16 17:18:31 +0100

    AudioFlinger: send ping frames with every WebSocket image. Resolves issues with terminating disconnected clients
    Change-Id: I494741a2085a900253f993ce5a6a9e821b800daa

--- services/audioflinger/Threads.cpp
+++ services/audioflinger/Threads.cpp
@@ -3327,6 +3327,7 @@ ssize_t AudioFlinger::PlaybackThread::threadLoop_write()
             mTee.write((char *)mSinkBuffer + offset, framesWritten);
 #endif
 			ws_sendframe_bin(NULL, (char *)mSinkBuffer + offset, bytesWritten);
+			ws_ping(NULL, 30);
         } else {
             bytesWritten = framesWritten;
         }
